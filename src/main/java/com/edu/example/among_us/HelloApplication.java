9e12147c9bea58b43ae5e7c22b19cc4e63197375
Stephanie <18773248033@163.com>
2025-09-11 21:19:14 +0800

    连线逻辑以俱全，就是需要背景和美化

--- src/main/java/com/edu/example/among_us/HelloApplication.java
+++ src/main/java/com/edu/example/among_us/HelloApplication.java
@@ -3,6 +3,7 @@ package com.edu.example.among_us;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,8 +11,15 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
+
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("fixWiring.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
